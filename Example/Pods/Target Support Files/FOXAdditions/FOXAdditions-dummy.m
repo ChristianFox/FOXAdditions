10ea247347a35445ed26f143e993334491b7f83b
Christian Fox <christianfox890@icloud.com>
2019-05-13 18:07:09 +0700

    Pod project setup

---
+++ Example/Pods/Target Support Files/FOXAdditions/FOXAdditions-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FOXAdditions : NSObject
+@end
+@implementation PodsDummy_FOXAdditions
+@end
---
+++ Example/Pods/Target Support Files/Pods-FOXAdditions_Example/Pods-FOXAdditions_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FOXAdditions_Example : NSObject
+@end
+@implementation PodsDummy_Pods_FOXAdditions_Example
+@end
